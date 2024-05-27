b2e6e088ed9682c8310a8234bc3ac10ab1b8316c
evelinechaza <evelinenokutendachaza@gmail.com>
2024-05-26 21:36:40 -0600

    pages made

---
+++ slideshow.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+let slideInterval;
+
+function initSlides() {
+    showSlides(slideIndex);
+    slideInterval = setInterval(() => {
+        plusSlides(1);
+    }, 5000); // Change image every 5 seconds
+}
+
+function plusSlides(n) {
+    clearInterval(slideInterval);
+    showSlides(slideIndex += n);
+    slideInterval = setInterval(() => {
+        plusSlides(1);
+    }, 5000);
+}
+
+function currentSlide(n) {
+    clearInterval(slideInterval);
+    showSlides(slideIndex = n);
+    slideInterval = setInterval(() => {
+        plusSlides(1);
+    }, 5000);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Initialize the slideshow
+window.onload = initSlides;
+
